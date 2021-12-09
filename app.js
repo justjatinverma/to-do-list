0b8bbe54dd314258b8700bdab8b1b768f5dbfaee
justjatinverma <vermajatin1727@gmail.com>
2021-12-09 21:29:01 +0530

    project-completed

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
-//Task class: represent a task
-class task {
-  constructor(title, desc) {
-    this.title = title;
-    this.desc = desc;
-  }
+//selectors
+const task =document.querySelector('#task') ;
+const todoButton =document.querySelector('#btn') ;
+const todoList =document.querySelector('#todo-list') ;
+
+// event listener
+todoButton.addEventListener("click",addToDo);
+todoList.addEventListener("click", deleteCheck);
+
+// function
+
+function addToDo(event) {
+    // prevent form from submitting
+    event.preventDefault();
+    // Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    // create li
+    const newToDo = document.createElement('li');
+    newToDo.innerText = task.value;
+    newToDo.classList.add('todo-item');
+    todoDiv.appendChild(newToDo);
+    // check mark
+    const complectedButton = document.createElement('button');
+    complectedButton.innerHTML = '<i class="fas fa-check"></i>';
+    complectedButton.classList.add("complete-btn");
+    todoDiv.appendChild(complectedButton);
+    // trash mark
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv)
+    task.value = " ";
 }
 
-//interface class
-class ui {
-  static displayTask() {
-    const storedTask = [
-      {
-        title: "home work",
-        desc: "collage work",
-      },
-      {
-        title: "home work",
-        desc: "collage work",
-      },
-    ];
+function deleteCheck(e) {
+    const item = e.target;
+    if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement
+        todo.remove();
+    }
 
-    const task = storedTask;
-
-    task.forEach((task) => ui.addTaskToList(task));
-  }
-
-  static addTaskToList(task) {
-    const list = document.querySelector("#task-list");
-
-     const row = document.createElement('tr')
-
-     row.innerHTML = `
-        <td>${task.title}</td>
-        <td>${task.desc}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-     `;
-     list.appendChild(row);
-  }
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+    }
 }
 
-// storage class
-
-//event to display book
-document.addEventListener('DOMContentLoaded',ui.displayTask)
-//add a book
-document.querySelector('#task-form').addEventListener('submit')
-//remove a book
